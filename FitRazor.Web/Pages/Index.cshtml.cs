57770335157f73317051797949d00100cc831b0c
ShamilShigapov <sshigapov1990@gmail.com>
2026-03-06 02:27:36 +0300

    добавил заглушку для фото тренеров на карточках

--- FitRazor.Web/Pages/Index.cshtml.cs
+++ FitRazor.Web/Pages/Index.cshtml.cs
@@ -1,13 +1,23 @@
+using FitRazor.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace FitRazor.Web.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly FitRazorContext _context;
+        public List<Trainer> Trainers { get; set; } = [];
 
+        public IndexModel(FitRazorContext context)
+        {
+            _context = context;
+        }
+
+        public async Task OnGetAsync()
+        {
+            Trainers = await _context.Trainers.ToListAsync();
         }
     }
 }
